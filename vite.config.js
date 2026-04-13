b6c4b70021fd711d2ac21c5201d0bee99c9e48f3
akmal maulana <am.maulana997@gmail.com>
2026-04-13 12:51:02 +0700

    deploy baru

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/akmalamang/warnasibumaryamah/',
+  base: '/warnasibumaryamah/',
   plugins: [react(), tailwindcss()],
 });
